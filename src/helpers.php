3b677d603f38aa3c8c76ab09c7f831e0e5a47858
atakansn <atkansenturk@gmail.com>
2022-06-10 20:48:15 +0300

    functions added

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,27 @@
 <?php
 
+
+function move_directory(string $from, string $to, bool $overwrite = false): bool
+{
+    return (new \Helper\FileSystem())->moveDirectory($from,$to,$overwrite);
+}
+
+function delete_directory(string $directory, bool $preserve = false): bool
+{
+    return (new \Helper\FileSystem())->deleteDirectory($directory,$preserve);
+}
+
+function clean_directory(string $directory): bool
+{
+    return (new \Helper\FileSystem())->cleanDirectory($directory);
+}
+
+function copy_directory(string $directory, string $destination, int|null $options): bool
+{
+    return (new \Helper\FileSystem())->copyDirectory($directory,$destination,$options);
+}
+
+function __symlink(string $target, string $link)
+{
+    return (new \Helper\FileSystem())->symlink($target,$link);
+}
